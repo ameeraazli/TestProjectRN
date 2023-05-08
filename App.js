7a93dda76beb0dd9612ff09f0a0155e930ff8c31
ameeraazli <ameera.azli@ytl.com>
2023-05-08 16:02:25 +0800

    read every item from the array - including key prop value - this one can only be used for the case of strings only

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHAndler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => <Text>{goal}</Text>)}
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
